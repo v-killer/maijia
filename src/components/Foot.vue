7f083451ed113ad266fe0a9cf1447bee524276cb
shen <343206898@qq.com>
2018-12-04 09:49:30 +0800

    底部导航跳转功能

--- src/components/Foot.vue
+++ src/components/Foot.vue
@@ -1,19 +1,47 @@
 <template>
         <div class="bottom-nav">
       <ul>
-        <li class="active"><a href="index.html"><i class="icon-home"></i><div>有赞</div></a></li>
-        <li><a href="category.html"><i class="icon-category"></i><div>分类</div></a></li>
-        <li><a href="cart.html"><i class="icon-cart"></i><div>购物车</div></a></li>
-        <li><a href="member.html"><i class="icon-user"></i><div>我</div></a></li>
+        <li :class="{active:index==curIndex}" v-for="(list,index) in navConfig"
+        @click="changeNav(list,index)"
+        ><a><i :class="list.icon"></i><div>{{list.name}}</div></a></li>
       </ul>
     </div>
 </template>
 
 <script>
+import qs from 'qs'
+
+let {index} = qs.parse(location.search.substring(1))
+
+let navConfig = [{
+  name: '有赞',
+  icon: 'icon-home',
+  href: 'index.html'
+},{
+  name: '分类',
+  icon: 'icon-category',
+  href: 'category.html'
+},{
+  name: '购物车',
+  icon: 'icon-cart',
+  href: 'cart.html'
+},{
+  name: '我',
+  icon: 'icon-user',
+  href: 'member.html'
+}]
+
 export default {
   data(){
     return {
-      
+      navConfig,
+      curIndex: parseInt(index) || 0
+    }
+  },
+  methods: {
+    changeNav(list,index){
+      // this.curIndex = index
+      location.href = `${list.href}?index=${index}`
     }
   }
 };
